1db58e6d1ab5fb2d3052bbd0ff3dad19f1df13c0
Amyr Haq <amyr@amyrhaq.com>
2013-10-31 16:51:05 -0700

    updated shirts.php

--- shirts.php
+++ shirts.php
@@ -1,12 +1,23 @@
-<?php 
+<?php include("inc/products.php"); ?><?php 
 $pageTitle = "Mike's Full Catalog of Shirts";
 $section = "shirts";
 include('inc/header.php'); ?>
 
-		<div class="section page">
+		<div class="section shirts page">
 
-			<h1>Mike&rsquo;s Full Catalog of Shirts</h1>
+			<div class="wrapper">
+
+				<h1>Mike&rsquo;s Full Catalog of Shirts</h1>
+
+				<ul class="products">
+					<?php foreach($products as $product_id => $product) { 
+							echo get_list_view_html($product_id,$product);
+						}
+					?>
+				</ul>
+
+			</div>
+
 		</div>
-
 
 <?php include('inc/footer.php') ?>
